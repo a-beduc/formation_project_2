066358df2d6395686dbd8ca2318ac87498622ea2
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-07-02 17:03:33 +0200

    added a function csv_loader in load_csv.py reformating on other files

--- module_book/create_dictionnaries.py
+++ module_book/create_dictionnaries.py
@@ -1,5 +1,6 @@
 from book import Book
 
+
 def create_dictionary(product_url):
     book = Book.from_url(product_url)
     book = vars(book)
--- module_book/load_csv.py
+++ module_book/load_csv.py
@@ -1,26 +1,36 @@
-from urls_scraper import get_product_pages_urls
 from create_dictionnaries import create_dictionary
 import csv
 
 
+def csv_loader(list_of_url, csv_name, batch_size=20):
+    if not isinstance(list_of_url, list):
+        list_of_url = [list_of_url]
+
+    csv_file = csv_name + ".csv"
+    header = create_dictionary(list_of_url[0])
+
+    with open(csv_file, 'a', newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=header.keys())
+        writer.writeheader()
+
+        current_batch = []
+
+        for url in list_of_url:
+            book = create_dictionary(url)
+            current_batch.append(book)
+            if len(current_batch) == batch_size:
+                writer.writerows(current_batch)
+                current_batch = []
+
+        if current_batch:
+            writer.writerows(current_batch)
+
+    return print("\n\ntravaille termine\n\n")
+
+
 def main():
-    url = "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
-    urls = get_product_pages_urls(url)
-
-    batch = []
-    for i in urls:
-        book = create_dictionary(i)
-        batch.append(book)
-
-    csv_variable_name = "automated_name_later" + ".csv"
-
-    with open(csv_variable_name, "w", newline="") as myfile:
-        wro = csv.DictWriter(myfile, fieldnames=batch[0].keys())
-        wro.writeheader()
-        for i in batch:
-            wro.writerow(i)
-
-    print("CSV LOADED")
+    url_2 = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+    csv_loader(url_2, "testunique")
 
 
 if __name__ == "__main__":
